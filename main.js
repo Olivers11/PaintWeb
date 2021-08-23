83fd50a8a35592a20c65750f02cdab281e65f1ac
= <olstertecn597@gmail.com>
2021-08-22 19:05:44 -0600

    fisrt version finished, the aplication have the methods for draw lines, squares, circles, draw like pencil and erase with the cursor, yeah this shit run well i'm my idol laptm

--- main.js
+++ main.js
@@ -30,7 +30,7 @@ document.getElementById('select').addEventListener('change', (e)=>{
 function changeMode(element) {
   
   mode = element.id;
-  if(mode == 3){
+  if(mode == 3 || mode == 1  || mode == 2){
     document.getElementById('canvas').style.cursor = "crosshair";
   }
   else{
@@ -55,6 +55,16 @@ function init() {
   canvas.height = height;
 
   canvas.addEventListener("mousedown", (e) => {
+    if(e.button == 2)
+    {
+      mode = 4;
+      document.getElementById('canvas').style.cursor = "url('Images/erase.ico')1 30, crosshair";
+    }
+    else if(mode == 4 && e.button == 0) 
+    {
+      mode = 0;
+      document.getElementById('canvas').style.cursor = "url('Images/paintbrush.ico')1 30, crosshair";
+    }
     mouse.drawing = true;
     mouse.click = true;
     mouse.pos.x = e.clientX;
@@ -88,19 +98,97 @@ function init() {
       mouse.pos.y = e.clientY;
       if(mouse.click)mouse.drawing = true;
     }
+    else if(mode == 4){
+      mouse.pos.x = e.clientX;
+      mouse.pos.y = e.clientY;
+      if(mouse.click)mouse.drawing = true;
+    }
+    else if(mode == 1 && mouse.click == true){
+      //borrar la primera linea xd
+      DrawLine(mouse.pos_prev.x,mouse.pos_prev.y, mouse.pos.x, mouse.pos_prev.y, "white");
+      DrawLine(mouse.pos_prev.x,mouse.pos_prev.y, mouse.pos.x, mouse.pos_prev.y-1, "white");
+      DrawLine(mouse.pos_prev.x,mouse.pos_prev.y, mouse.pos.x, mouse.pos_prev.y+1, "white");
+      DrawLine(mouse.pos_prev.x,mouse.pos_prev.y-1, mouse.pos.x, mouse.pos_prev.y, "white");
+      DrawLine(mouse.pos_prev.x,mouse.pos_prev.y+1, mouse.pos.x, mouse.pos_prev.y, "white");
+
+
+      //segunda linda
+      DrawLine(mouse.pos_prev.x,mouse.pos.y, mouse.pos.x, mouse.pos.y, "white");
+      DrawLine(mouse.pos_prev.x,mouse.pos.y, mouse.pos.x, mouse.pos.y-1, "white");
+      DrawLine(mouse.pos_prev.x,mouse.pos.y, mouse.pos.x, mouse.pos.y+1, "white");
+      DrawLine(mouse.pos_prev.x,mouse.pos.y-1, mouse.pos.x, mouse.pos.y, "white");
+      DrawLine(mouse.pos_prev.x,mouse.pos.y+1, mouse.pos.x, mouse.pos.y, "white");
+      
+      //tercera linea xd
+      DrawLine(mouse.pos_prev.x,mouse.pos_prev.y, mouse.pos_prev.x, mouse.pos.y, "white");
+      DrawLine(mouse.pos_prev.x,mouse.pos_prev.y, mouse.pos_prev.x, mouse.pos.y-1, "white");
+      DrawLine(mouse.pos_prev.x,mouse.pos_prev.y, mouse.pos_prev.x, mouse.pos.y+1, "white");
+      DrawLine(mouse.pos_prev.x,mouse.pos_prev.y-1, mouse.pos_prev.x, mouse.pos.y, "white");
+      DrawLine(mouse.pos_prev.x,mouse.pos_prev.y+1, mouse.pos_prev.x, mouse.pos.y, "white");
+
+
+      //cuarta linea xd
+      DrawLine(mouse.pos.x,mouse.pos_prev.y, mouse.pos.x, mouse.pos.y, "white");
+      DrawLine(mouse.pos.x,mouse.pos_prev.y, mouse.pos.x, mouse.pos.y-1, "white");
+      DrawLine(mouse.pos.x,mouse.pos_prev.y, mouse.pos.x, mouse.pos.y+1, "white");
+      DrawLine(mouse.pos.x,mouse.pos_prev.y-1, mouse.pos.x, mouse.pos.y, "white");
+      DrawLine(mouse.pos.x,mouse.pos_prev.y+1, mouse.pos.x, mouse.pos.y, "white");
+
+
+
+
+      mouse.pos.x = e.clientX;
+      mouse.pos.y = e.clientY;
+      DrawLine(mouse.pos_prev.x,mouse.pos_prev.y, mouse.pos.x, mouse.pos_prev.y, color);
+      DrawLine(mouse.pos_prev.x,mouse.pos.y, mouse.pos.x, mouse.pos.y, color);
+      DrawLine(mouse.pos_prev.x,mouse.pos_prev.y, mouse.pos_prev.x, mouse.pos.y, color);
+      DrawLine(mouse.pos.x,mouse.pos_prev.y, mouse.pos.x, mouse.pos.y, color);
+
+      if(mouse.click)mouse.drawing = true;
+    }
+    else if(mode == 2 && mouse.click == true)
+    {
+
+      DrawCircle(mouse.pos.x, mouse.pos.y-1, Distance(mouse.pos_prev.x, mouse.pos_prev.y, mouse.pos.x, mouse.pos.y), "white");
+      DrawCircle(mouse.pos.x, mouse.pos.y+1, Distance(mouse.pos_prev.x, mouse.pos_prev.y, mouse.pos.x, mouse.pos.y), "white");
+      DrawCircle(mouse.pos.x+1, mouse.pos.y, Distance(mouse.pos_prev.x, mouse.pos_prev.y, mouse.pos.x, mouse.pos.y), "white");
+      DrawCircle(mouse.pos.x-1, mouse.pos.y, Distance(mouse.pos_prev.x, mouse.pos_prev.y, mouse.pos.x, mouse.pos.y), "white");
+      
+      mouse.pos.x = e.clientX;
+      mouse.pos.y = e.clientY;
+      DrawCircle(mouse.pos.x, mouse.pos.y, Distance(mouse.pos_prev.x, mouse.pos_prev.y, mouse.pos.x, mouse.pos.y), color);
+      if(mouse.click)mouse.drawing = true;
+    }
     
   });
 
-  function Draw(x, y, radius) {
+  function Distance(x1, y1, x2, y2)
+  {
+    var r1 = (x2-x1)*(x2-x1);
+    var r2 = (y2-y1)*(y2-y1);
+    var suma = r1+r2;
+    return Math.pow(suma, 1/2);
+  }
+
+  function Draw(x, y, radius, c) {
     context.beginPath();
     context.arc(x, y-52, radius, 0, 2 * Math.PI, false);
-    context.fillStyle = color;
+    context.fillStyle = c;
     context.fill();
     context.lineWidth = 2;
-    context.strokeStyle = color;
+    context.strokeStyle = c;
     context.stroke();
-
   }
+
+  function DrawCircle(x, y, radius, c) {
+    context.beginPath();
+    context.arc(x, y-52, radius, 0, 2 * Math.PI, false);
+    context.lineWidth = size;
+    context.strokeStyle = c;
+    context.stroke();
+  }
+
+
 
   function DrawLine(p_x, p_y, x, y, c){
     context.beginPath();
@@ -116,11 +204,10 @@ function init() {
     if(mouse.drawing){
       if(mode == 0)
       {
-        Draw(mouse.pos.x, mouse.pos.y, size);
+        Draw(mouse.pos.x, mouse.pos.y, size, color);
       }
-      else if(mode == 3)
-      {
-        console.log(mouse.pos_prev);
+      else if(mode == 4){
+        Draw(mouse.pos.x, mouse.pos.y, 5, "white");
       }
       mouse.drawing = false;
     }
